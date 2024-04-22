fe2608b147688e594f88619639165c1f3adaadca
Haozhuang Chi <chi1996705@outlook.com>
2024-04-22 12:05:12 +0800

    correct dependencies for lora llava;

--- finetune_task_lora_DSLLM-test.sh
+++ finetune_task_lora_DSLLM-test.sh
@@ -1,35 +1,5 @@
 #!/bin/bash
 
-### The following requests all resources on 1 DGX-1 node
-#PBS -l select=1:ngpus=4:ncpus=16
-
-### Specify amount of time required
-#PBS -l walltime=48:00:00
-
-### Specify project code
-#PBS -P 12002486
-
-### Specify name for job
-#PBS -N lora_1.6-7b
-
-### Standard output by default goes to file $PBS_JOBNAME.o$PBS_JOBID
-### Standard error by default goes to file $PBS_JOBNAME.e$PBS_JOBID
-### To merge standard output and error use the following
-#PBS -j oe
-
-### For automatic mailing, use the following options:
-#PBS -M chih0001@e.ntu.edu.sg
-#PBS -m abe
-
-### Start of commands to be run
-source /home/users/ntu/chih0001/anaconda3/etc/profile.d/conda.sh
-conda activate llava-test
-
-export CUDA_VISIBLE_DEVICES=0,1,2,3
-
-cd /home/users/ntu/chih0001/scratch/VLM/LLaVA
-
-# Run deepspeed training command directly
 deepspeed llava/train/train_mem.py \
     --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
     --deepspeed ./scripts/zero3.json \
