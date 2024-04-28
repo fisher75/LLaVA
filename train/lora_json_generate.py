9df97be6375a0480460f2b351578706c93fb7c84
Haozhuang Chi <chi1996705@outlook.com>
2024-04-28 16:54:46 +0800

    add global batch size;

--- train/lora_json_generate.py
+++ train/lora_json_generate.py
@@ -14,26 +14,46 @@ def generate_sample(image_path, task, question, response):
 
 def collect_samples(base_dir, task, question_template, label_mapping):
     samples = []
+    total_images = 0
+    used_images = 0
+    
     for root, dirs, files in os.walk(base_dir):
         for file in files:
             if file.endswith('.jpg') or file.endswith('.jpeg'):
-                label = file.split('_')[0]  # Modify this depending on file naming convention
+                total_images += 1
+                label = None
+                
+                if task == "distraction":
+                    label = file.split('_')[0]
+                elif task == "emotion":
+                    label = file.split('_')[1]
+                elif task == "drowsiness":
+                    label = file.split('_')[-1].split('.')[0]
+                
                 if label in label_mapping:
+                    used_images += 1
                     question = question_template
                     response = f"Driver is {label_mapping[label]}."
                     full_image_path = os.path.join(root, file)
                     sample = generate_sample(full_image_path, task, question, response)
                     samples.append(sample)
+    
+    print(f"Total images in {base_dir}: {total_images}")
+    print(f"Used images in {base_dir}: {used_images}")
+    
     return samples
+
+
+
 
 def main():
     distraction_dir = "/home/users/ntu/chih0001/scratch/data/distraction/SAM-DD/train"
     emotion_dir = "/home/users/ntu/chih0001/scratch/data/emotion/FED_2/train"
     drowsiness_dir = "/home/users/ntu/chih0001/scratch/data/drowsiness/NTHUDDD_2/train"
 
-    distraction_question = "\n**Identify Driver Behavior from Image**\n\n**Task:** What behavior is the driver exhibiting?\n\n**Options:**\n\n- Normal Driving\n- Drinking\n- Phoning Left\n- Phoning Right\n- Texting Left\n- Texting Right\n- Touching Hairs & Makeup\n- Adjusting Glasses\n- Reaching Behind\n- Dropping\n\n**Instructions:** Analyze the image and identify the driver's behavior."
-    emotion_question = "Analyze the person's facial expression in the uploaded image. Look closely at the facial features such as the eyebrows, eyes, mouth, and overall facial tension."
-    drowsiness_question = "This image captures a crucial moment in a driver's journey. Your task is to observe the driver's posture, facial expression, and overall appearance carefully."
+    distraction_question = "<image>\n**Identify Driver Behavior from Image**\n\n**Task:** What behavior is the driver exhibiting?\n\n**Options:**\n\n- Normal Driving\n- Drinking\n- Phoning Left\n- Phoning Right\n- Texting Left\n- Texting Right\n- Touching Hairs & Makeup\n- Adjusting Glasses\n- Reaching Behind\n- Dropping\n\n**Instructions:** Analyze the image and identify the driver's behavior."
+    emotion_question = "<image>\nAnalyze the person's facial expression in the uploaded image. Look closely at the facial features such as the eyebrows, eyes, mouth, and overall facial tension."
+    drowsiness_question = "<image>\nThis image captures a crucial moment in a driver's journey. Your task is to observe the driver's posture, facial expression, and overall appearance carefully."
 
     distraction_labels = {
         "0": "Normal Driving", "1": "Drinking", "2": "Phoning Left", "3": "Phoning Right",
